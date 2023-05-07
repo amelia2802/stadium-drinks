642d1c73902dfc7e97c872554d75dd513064930e
Amelia Dutta <amelia280111dutta@gmail.com>
2023-05-07 15:16:27 +0530

    made the slider automatic

--- assests/js/script.js
+++ assests/js/script.js
@@ -19,7 +19,21 @@ function dot(index){
     document.getElementById("content" + index).classList.add("active");
     currentSlide = index - 1;
     indicator(index);
+    
 }
+
+function carousel() {
+  var i;
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";  
+  }
+  currentSlide++;
+  if (currentSlide > images.length) {currentSlide = 1}    
+  images[currentSlide-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+carousel()
 
 function contactForm(){
     document.getElementById("contact").style.display="flex";
